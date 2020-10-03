2f299bef26fdfc7e86e9114e2c230a423b5af957
diegolopez03 <diego232.lopez@gmail.com>
2020-10-03 01:37:54 -0600

    Implementación de express-handlebars como template engine y creación del endpoint de / con el formulario de préstamo

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Importar express
 const express = require("express");
-/* 
 // Importar handlebars como template engine
 const exphbs = require("express-handlebars");
+/*
 // Importar body parser que nos permite acceder al cuerpo de la petición HTTP
 const bodyParser = require("body-parser");
 // Importar la función de cálculo de método francés
@@ -12,22 +12,22 @@ const { calcularMetodoFrances } = require("./calculoMetodoFrances");
 // Crear un servidor express
 const app = express();
 
-/*
+
 // Indicar a express utilizar handlebars como template engine
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 
 app.set("view engine", "hbs");
-
+/*
 // Habilitar body parse para leer los datos del cuerpo de peticiones POST
 app.use(bodyParser.urlencoded({ extended: true }));
-
+*/
 // Crear una ruta para /
 // Información sobre los verbos HTTP
 // https://developer.mozilla.org/es/docs/Web/HTTP/Methods
 app.get("/", (req, res, next) => {
   res.render("formulario_prestamo");
 });
-
+/*
 app.post("/prestamo", (req, res, next) => {
   // Asignación por destructuring
   // https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Operadores/Destructuring_assignment
