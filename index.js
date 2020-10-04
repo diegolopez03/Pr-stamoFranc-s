78ca47389a5e50f55cc5188f423ed238e4c5a64b
diegolopez03 <diego232.lopez@gmail.com>
2020-10-03 20:28:10 -0600

    Cálculo de préstamo por método francés

--- index.js
+++ index.js
@@ -2,13 +2,12 @@
 const express = require("express");
 // Importar handlebars como template engine
 const exphbs = require("express-handlebars");
-/*
+
 // Importar body parser que nos permite acceder al cuerpo de la petición HTTP
 const bodyParser = require("body-parser");
+
 // Importar la función de cálculo de método francés
 const { calcularMetodoFrances } = require("./calculoMetodoFrances");
-*/
-
 // Crear un servidor express
 const app = express();
 
@@ -17,17 +16,17 @@ const app = express();
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
 
 app.set("view engine", "hbs");
-/*
+
 // Habilitar body parse para leer los datos del cuerpo de peticiones POST
 app.use(bodyParser.urlencoded({ extended: true }));
-*/
+
 // Crear una ruta para /
 // Información sobre los verbos HTTP
 // https://developer.mozilla.org/es/docs/Web/HTTP/Methods
 app.get("/", (req, res, next) => {
   res.render("formulario_prestamo");
 });
-/*
+
 app.post("/prestamo", (req, res, next) => {
   // Asignación por destructuring
   // https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Operadores/Destructuring_assignment
@@ -37,7 +36,7 @@ app.post("/prestamo", (req, res, next) => {
 
   res.send("Petición aceptada");
 });
-*/
+
 
 // Inicializar el servidor en un puerto en específico
 app.listen(5000, () => {
